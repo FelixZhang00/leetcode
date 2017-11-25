0dc780f5dcb0ca03a27c0c74577c0afb5a063f28
Jamffy <l836828946@gmail.com>
2017-11-25 21:42:18 +0800

    problem 507

---
+++ src/leetcode/Math_507/Solution.java
@@ -0,0 +1,31 @@
+package leetcode.Math_507;
+
+/**
+ * Created by felix on 2017/11/25.
+ */
+public class Solution {
+
+  public boolean checkPerfectNumber(int num) {
+    if (num == 1 || num == 0) {
+      return false;
+    }
+    int sum = 1;
+    int start = 2;
+
+    while (start <= Math.sqrt(num)) {
+      if (num % start == 0) {
+        sum += start;
+        sum += num / start;
+      }
+      start++;
+    }
+
+    return sum == num;
+  }
+
+  public static void main(String args[]) {
+    Solution solution = new Solution();
+    boolean perfectNumber = solution.checkPerfectNumber(28);
+    System.out.println("perfectNumber+" + perfectNumber);
+  }
+}
